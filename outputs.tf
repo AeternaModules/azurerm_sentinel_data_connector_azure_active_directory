2ddd02160cd3751c7d3423f9264e8bf812e47fb7
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:54 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "sentinel_data_connector_azure_active_directories" {
   description = "All sentinel_data_connector_azure_active_directory resources"
   value       = azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories
 }
+output "sentinel_data_connector_azure_active_directories_log_analytics_workspace_id" {
+  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_azure_active_directories"
+  value       = [for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : v.log_analytics_workspace_id]
+}
+output "sentinel_data_connector_azure_active_directories_name" {
+  description = "List of name values across all sentinel_data_connector_azure_active_directories"
+  value       = [for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : v.name]
+}
+output "sentinel_data_connector_azure_active_directories_tenant_id" {
+  description = "List of tenant_id values across all sentinel_data_connector_azure_active_directories"
+  value       = [for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : v.tenant_id]
+}
 
