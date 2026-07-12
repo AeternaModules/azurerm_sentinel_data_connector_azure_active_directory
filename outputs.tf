42f959227bc54ed17b9458cc20b9fbb27968dbe3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "sentinel_data_connector_azure_active_directories_id" {
+  description = "Map of id values across all sentinel_data_connector_azure_active_directories, keyed the same as var.sentinel_data_connector_azure_active_directories"
+  value       = { for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : k => v.id }
+}
 output "sentinel_data_connector_azure_active_directories_log_analytics_workspace_id" {
   description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_azure_active_directories, keyed the same as var.sentinel_data_connector_azure_active_directories"
   value       = { for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : k => v.log_analytics_workspace_id }
