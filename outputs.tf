8c38017d917bcaec4c883d61396796850d55a1fb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_azure_active_directories" {
-  description = "All sentinel_data_connector_azure_active_directory resources"
-  value       = azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories
-}
 output "sentinel_data_connector_azure_active_directories_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_azure_active_directories"
-  value       = [for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_azure_active_directories, keyed the same as var.sentinel_data_connector_azure_active_directories"
+  value       = { for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_azure_active_directories_name" {
-  description = "List of name values across all sentinel_data_connector_azure_active_directories"
-  value       = [for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : v.name]
+  description = "Map of name values across all sentinel_data_connector_azure_active_directories, keyed the same as var.sentinel_data_connector_azure_active_directories"
+  value       = { for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : k => v.name }
 }
 output "sentinel_data_connector_azure_active_directories_tenant_id" {
-  description = "List of tenant_id values across all sentinel_data_connector_azure_active_directories"
-  value       = [for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : v.tenant_id]
+  description = "Map of tenant_id values across all sentinel_data_connector_azure_active_directories, keyed the same as var.sentinel_data_connector_azure_active_directories"
+  value       = { for k, v in azurerm_sentinel_data_connector_azure_active_directory.sentinel_data_connector_azure_active_directories : k => v.tenant_id }
 }
 
